753f6b1f5b64a0b6827ec52916ebe2f19e87330f
Fernando <motercode@gmail.com>
2022-02-18 19:55:49 +0100

    integration test template finished

--- src/mybot.js
+++ src/mybot.js
@@ -1,14 +1,26 @@
 import { Telegraf } from 'telegraf';
+import 'dotenv/config'
 
 class MyBot{
     constructor(){
-        this.bot = new Telegraf('fake');
+        this.bot = new Telegraf(process.env.BOT_TOKEN);
+        this.registreCommands();
     }
 
 
     launch(){
         this.bot.launch();
     }
+
+    registreCommands(){
+        this.bot.on('text', (ctx) => {        
+            // Using context shortcut
+            ctx.reply(`pong`)
+          })
+    }
+
+  
+
 }
 
 export { MyBot };
--- test/client_integration.test.js
+++ test/client_integration.test.js
@@ -2,12 +2,26 @@ import { TelegramClient } from "telegram";
 import { StringSession } from "telegram/sessions";
 import input from "input";
 import 'dotenv/config'
-// const { Api } = require("telegram/tl");
+
+// clase a testear
+import { MyBot } from '../src/mybot.js';
 
 
-const apiId = process.env.API_ID;
+
+const apiId = parseInt(process.env.API_ID);
 const apiHash = process.env.API_HASH;
 const stringSession = new StringSession(process.env.STRING_SESSION); // fill this later with the value from session.save()
+
+
+let resultMessage=[];
+const timeoutsecs = 3;
+
+const messageExpected = "pong";
+const intakeMessage = "ping";
+let aBot = new MyBot();
+aBot.launch();
+
+
 
 (async () => {
   console.log("Loading interactive example...");
@@ -21,10 +35,35 @@ const stringSession = new StringSession(process.env.STRING_SESSION); // fill thi
       await input.text("Please enter the code you received: "),
     onError: (err) => console.log(err),
   });
-  console.log("You should now be connected.");
-  console.log(client.session.save()); // Save this string to avoid logging in again
-  await client.sendMessage("espesa_bot", { message: "Hello!" });
+  // console.log("You should now be connected.");
+  // console.log(client.session.save()); // Save this string to avoid logging in again
+  
+  
 
+  let lastmessage=undefined;
+  let timeOut = false;
+
+  await client.sendMessage(process.env.NAME_OF_BOT_CHANNEL, { message: intakeMessage });
+  const sentIntakeMessageTime =  Date.now();
+  let testeando = 0
+  while (lastmessage!=messageExpected && !timeOut) {
+    testeando =Date.now() - sentIntakeMessageTime;
+    timeOut = (( Date.now() - sentIntakeMessageTime) > (timeoutsecs*1000));
+    lastmessage = (await client.getMessages(process.env.NAME_OF_BOT_CHANNEL,{}))[0];
+   
+  }
+
+  if(lastmessage.message=messageExpected)
+  {
+     resultMessage.push('el test ha sido correcto');
+  }
+  else{
+    resultMessage.push(`timeout no ha contestado el bot o ha tardado mas de ${timeoutsecs} segundos `);
+  }
+
+
+  console.log(resultMessage);
+  process.exit(1);
 
 })(); 
 
